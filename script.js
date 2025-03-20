e4435f35383c43093a79e91200798dd52290d256
JohnFerrancol <88333166+JohnFerrancol@users.noreply.github.com>
2025-03-20 15:42:16 +0800

    Set up hover effect so that the grid divs change color when you mouse over them

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const gridContainer = document.querySelector(".grid-container");
 
+// Creating the 16x16 grid by adding 256 of such grid elements
 for (let i = 0; i < 256; i++) {
-  let gridItem = document.createElement("div");
+  const gridItem = document.createElement("div");
   gridItem.classList.add("grid-item");
   gridContainer.appendChild(gridItem);
 }
+
+// By using event delegration, color the grid black if it moused over
+gridContainer.addEventListener("mouseover", (event) => {
+  event.target.style.backgroundColor = "black";
+});
