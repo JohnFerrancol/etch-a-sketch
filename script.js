04bb9dc9ac39e28744e4bb48f9de79558ff0e0ef
JohnFerrancol <88333166+JohnFerrancol@users.noreply.github.com>
2025-03-20 20:56:53 +0800

    Refactored code

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const gridContainer = document.querySelector(".grid-container");
 
-// Creating the 16x16 grid by adding 256 of such grid elements
+// Creating a 16x16 requires 256 grids, this can be done using a for loop
 for (let i = 0; i < 256; i++) {
+  // Create gridItem div and add it to the gridContainer as a child
   const gridItem = document.createElement("div");
   gridItem.classList.add("grid-item");
   gridContainer.appendChild(gridItem);
 }
 
-// By using event delegration, color the grid black if it moused over
+// Add an event delegation that changes the background color of the grid to black when mousedover
 gridContainer.addEventListener("mouseover", (event) => {
-  event.target.style.backgroundColor = "black";
+  let gridTarget = event.target;
+  // If the element moused over has a grid-item class then change the color
+  if (gridTarget.matches(".grid-item")) {
+    gridTarget.style.backgroundColor = "black";
+  }
 });
