ef32b33dacd90285229a3858ca417237d3b2e4ef
JohnFerrancol <88333166+JohnFerrancol@users.noreply.github.com>
2025-03-21 11:20:45 +0800

    Dynamically change the grid size and added a way to clear the current sketch

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 const gridContainer = document.querySelector(".grid-container");
+const displayGridSize = document.querySelector(".display");
 
-// Creating a 16x16 requires 256 grids, this can be done using a for loop
-for (let i = 0; i < 256; i++) {
-  // Create gridItem div and add it to the gridContainer as a child
-  const gridItem = document.createElement("div");
-  gridItem.classList.add("grid-item");
-  gridContainer.appendChild(gridItem);
-}
+window.addEventListener("load", createGrid(16));
 
 // Add an event delegation that changes the background color of the grid to black when mousedover
 gridContainer.addEventListener("mouseover", (event) => {
@@ -15,4 +10,39 @@ gridContainer.addEventListener("mouseover", (event) => {
   if (gridTarget.matches(".grid-item")) {
     gridTarget.style.backgroundColor = "black";
   }
+});
+
+const range = document.querySelector("#range");
+
+range.addEventListener("input", () => createGrid(range.value));
+
+function createGrid(gridSize) {
+  const gridDivs = document.querySelectorAll(".grid-item");
+
+  gridDivs.forEach((grid) => {
+    grid.remove();
+  });
+
+  const containerSize = 960;
+  let cellSize = containerSize / gridSize;
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.style.width = `${cellSize}px`;
+    gridItem.style.height = `${cellSize}px`;
+    gridItem.classList.add("grid-item");
+    gridContainer.appendChild(gridItem);
+  }
+
+  displayGridSize.textContent = `${gridSize} x ${gridSize}`;
+}
+
+const clearButton = document.querySelector("#clear");
+
+clearButton.addEventListener("click", () => {
+  const gridDivs = document.querySelectorAll(".grid-item");
+
+  gridDivs.forEach((grid) => {
+    grid.style.backgroundColor = "white";
+  });
 });
